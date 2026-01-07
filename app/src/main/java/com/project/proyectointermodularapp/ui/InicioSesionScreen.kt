e276f8445118d51c0f76d5d8cb0763269264e005
pablomorillas <pablomorillasesteve@gmail.com>
2026-01-07 20:25:18 +0100

    Inicio de sesión

---
+++ app/src/main/java/com/project/proyectointermodularapp/ui/InicioSesionScreen.kt
@@ -0,0 +1,95 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.project.proyectointermodularapp.R
+
+@Composable
+fun LoginScreen(
+    onLoginClick: (String, String) -> Unit
+) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo_place_holder),
+            contentDescription = "Logo",
+            modifier = Modifier.padding(bottom = 30.dp)
+        )
+
+        Text(
+            text = "Iniciar Sesión",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(30.dp),
+        )
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuario") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = { onLoginClick(username, password) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Iniciar sesión")
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun LoginScreenPreview() {
+    MaterialTheme {
+        LoginScreen { username, password ->
+            println("Usuario: $username, Password: $password")
+        }
+    }
+}
---
+++ app/src/main/java/com/project/proyectointermodularapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.project.proyectointermodularapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red = Color(0xFFC81619)
+
+val AlmosWhite = Color(0xFFF4F6F8)
+
+val Grey = Color(0xFF828282)
+
+val Black = Color(0xFF000000)
