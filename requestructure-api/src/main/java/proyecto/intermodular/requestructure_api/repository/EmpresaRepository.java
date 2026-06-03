f5a62a40739e2fee4ed34c243d1398d58db63987
Pablo <pablomorillasesteve@gmail.com>
2026-06-03 02:32:30 +0200

    fix(api): add missing Optional import in EmpresaRepository

--- requestructure-api/src/main/java/proyecto/intermodular/requestructure_api/repository/EmpresaRepository.java
+++ requestructure-api/src/main/java/proyecto/intermodular/requestructure_api/repository/EmpresaRepository.java
@@ -3,6 +3,8 @@ package proyecto.intermodular.requestructure_api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import proyecto.intermodular.requestructure_api.domain.Empresa;
 
+import java.util.Optional;
+
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
     boolean existsByEmail(String email);
     boolean existsByNif(String nif);
